2b8cf7cbffddede3658fdeeae9635ba2acf5a948
Tommy Cheng <yihhwa.cheng@broadcom.com>
2022-10-07 21:33:52 +0000

    base: smrepo now allows anonymous helm repo access

--- base/smrepo.sh
+++ base/smrepo.sh
@@ -2,10 +2,14 @@
 MYPATH=$(cd $(dirname "$0"); pwd)
 . "${MYPATH}/env.shlib"
 if [[ -z "$(repoexist "$SMREPO")" ]] ; then
-    helm repo add "$SMREPO" "$SMURL" \
-	--username "$SMID" \
-	--password "$SMPWD" \
-	--pass-credentials  
+    if [[ -z "$SMID" ]] ; then
+        helm repo add "$SMREPO" "$SMURL"
+    else
+        helm repo add "$SMREPO" "$SMURL" \
+	    --username "$SMID" \
+	    --password "$SMPWD" \
+	    --pass-credentials  
+    fi
 else
     >&2 echo repo $SMREPO exists
 fi
