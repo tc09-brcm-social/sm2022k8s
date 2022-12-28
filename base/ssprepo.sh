c5cd169ad94526752bb8dcbfd350869b33528cae
Tommy Cheng <yihhwa.cheng@broadcom.com>
2022-12-28 13:36:20 +0000

    base: ssprepo does not use ID and Password

--- base/ssprepo.sh
+++ base/ssprepo.sh
@@ -2,14 +2,7 @@
 MYPATH=$(cd $(dirname "$0"); pwd)
 . "${MYPATH}/env.shlib"
 if [[ -z "$(repoexist "$SSPREPO")" ]] ; then
-    if [[ -z "$SSPID" ]] ; then
-        helm repo add "$SSPREPO" "$SSPURL"
-    else
-        helm repo add "$SSPREPO" "$SSPURL" \
-	    --username "$SSPID" \
-	    --password "$SSPPWD" \
-	    --pass-credentials  
-    fi
+    helm repo add "$SSPREPO" "$SSPURL"
 else
     >&2 echo repo $SSPREPO exists
 fi
