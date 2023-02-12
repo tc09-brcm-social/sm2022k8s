64330eb7ee272514f648692dddef9a84a8bf990a
Tommy Cheng <yihhwa.cheng@broadcom.com>
2023-02-12 13:45:30 +0000

    envs/ahdemo: patchenv

---
+++ envs/ahdemo/patchenv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+MYPATH=$(cd $(dirname "$0"); pwd)
+cd "${MYPATH}"
+. ./env.shlib
+FILE="$1"
+patchenv() {
+    if [[ -z "$FILE" ]] ; then
+	echo "$1"=\"$2\"
+    else
+        x=$(bash "$MYPATH/../../tools/setkeyvalue.sh" "$FILE" "$1" "$2")
+        >&2 echo $1
+    fi
+    }
+
+LINES="$(grep '^[^# ]*=' env.shlib | sed 's/=.*$//')"
+for i in $LINES ; do
+    patchenv "$i" "$(eval 'echo $'$i)"
+done
