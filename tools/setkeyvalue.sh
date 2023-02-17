421e0fcbb1b772e0a6fa3ee382e08cdb00f42372
Tommy Cheng <yihhwa.cheng@broadcom.com>
2023-02-17 00:01:44 +0000

    tools: setkeyvalue now accepts fourth argument as a quote

--- tools/setkeyvalue.sh
+++ tools/setkeyvalue.sh
@@ -2,8 +2,12 @@
 FILE=$1
 KEY=$2
 VALUE=$3
+QUOTE=$4
+if [[ -z "$QUOTE" ]] ; then
+    QUOTE='"'	
+fi
 if ! grep -qs "^${KEY}=" "$FILE" ; then
     echo "$KEY=" >> "$FILE"
 fi
-sed -i -e "s#^${KEY}=.*#${KEY}=\"${VALUE}\"#" "${FILE}"
+sed -i -e "s#^${KEY}=.*#${KEY}=${QUOTE}${VALUE}${QUOTE}#" "${FILE}"
 echo "${VALUE}"
