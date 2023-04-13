6ef3e1d7b6a0880565425785d03e2a4c4b7b12b3
Tommy Cheng <yihhwa.cheng@broadcom.com>
2023-04-13 20:53:53 +0000

    envs/common/sminfra: values now accepts optional SMVER

--- envs/common/sminfra/values.sh
+++ envs/common/sminfra/values.sh
@@ -5,4 +5,7 @@ cd "${MYPATH}"
 if [[ ! -z "$VERSHLIB" ]] ; then
     . "${MYPATH}/../../../base/$VERSHLIB"
 fi
+if [[ ! -z "$1" ]] ; then
+    SMVER="$1"
+fi
 helm show values $SMREPO/siteminder-infra $SMVER
