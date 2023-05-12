63bc694bdd9debe1e4f7b93c740c7350ca3270d0
Tommy Cheng <yihhwa.cheng@broadcom.com>
2023-05-12 18:32:54 +0000

    upgrade: smadmin revisited

--- upgrade/smadmin.sh
+++ upgrade/smadmin.sh
@@ -24,22 +24,49 @@ psrelpreq() {
 	>&2 echo upgrade to version not specified
 	echo 1
     else
-        bash "${MYPATH}/ps/values.sh"  > "$PSVALUES"
+        bash "${MYPATH}/../envs/common/sc/rtvalues.sh"  > "$PSVALUES"
 	helm search repo "$SMREPO" --versions -o json \
 	    | jq '[.[] | select(.name == "'$SMREPO/server-components'")]' \
 	    > "$HELMSEARCH"
+	pstag="$(yq -r '.policyServer.tag' "$PSVALUES")"
 	tag="$(jq -r '.[] | select(.version == "'$_ver'") | .app_version' "$HELMSEARCH")"
 	newtag="$(jq -r '.[] | select(.version == "'$_newver'") | .app_version' "$HELMSEARCH")"
-	envbase SMTAG "$tag"
-	envbase SMNEWTAG "$newtag"
 	tag0="$(yq -r '.admin.adminUI.tag' "$PSVALUES")"
-	if [[ "$tag" = "$tag0" ]] ; then
+	if [[ ! "$pstag" = "$newtag" ]] ; then
+	    >&2 echo Policy Server Pod $pstag has not yet upgraded to new version $newtag
+	    echo 1
+	elif [[ "$tag" = "$tag0" ]] ; then
+	    envbase SMTAG "$tag"
+	    envbase SMNEWTAG "$newtag"
 	    echo 0
 	else
 	    >&2 echo runtime version $tag0 does not match with environment version $tag
 	    echo 1
 	fi
     fi
+    }
+
+#
+## upgradeadmin
+#
+upgradeadmin() {
+    ADMINS="$(yq -r '.admin.replicas' "$PSVALUES")"
+    ADMINR="$(bash "$MYPATH/../op/admin/containers.sh" | jq 'length')"
+    if (( $ADMINR < 2 )) ; then
+        bash "$MYPATH/../op/admin/scale.sh" 1
+    fi
+    if (( $ADMINS < 2 )) ; then
+        ADMINS=2
+    fi
+    >&2 echo $ADMINS $ADMINR
+    envbase ADMINS "$ADMINS"
+    cat "$PSVALUES" | \
+	bash "$MYPATH/../envs/common/sc/upgradeadmin1.sh" > "$PSVALUES".0
+    bash "$MYPATH/../envs/common/sc/newvalues.sh" > "$PSVALUES".1
+    helm upgrade "$PSREL" "$SMREPO/server-components" -n "$PSNS" \
+        -f "$PSVALUES".1 -f "$PSVALUES".0 $SMVERNEW \
+        --description="upgrade administrative server pod to $SMVERNEW" \
+        --timeout=30m --wait --debug > "$PSVALUES".debug
     }
 
 #
@@ -53,35 +80,19 @@ createns "$PSNS"
 #
 if [[ -z "$(relexist "$PSNS" "$PSREL")" ]] ; then
     >&2 echo SiteMinder Server Component release $PSREL is not installed in namespace $PSNS
+    exit 1
+fi
+if [[ "$(bash "$MYPATH/../envs/common/sc/rtvalues.sh" | yq -r '.admin.enabled')" != \
+    "true" ]] ; then
+    >&2 echo SiteMinder Administrative Server Pod is not enabled, upgrade skipped.
+    exit
+fi
+PSVALUES="$MYPATH/ps.$$.values.yaml"
+HELMSEARCH="$MYPATH/ps.$SMREPO.$$.json"
+if [ "$(psrelpreq "$(echo "$SMVER" | cut -f2 -d=)" \
+        "$(echo "$SMVERNEW" | cut -f2 -d=)")" -eq 0 ] ; then
+    >&2 echo to upgrade
+    upgradeadmin
 else
-    PSVALUES="$MYPATH/ps.$$.values.yaml"
-    HELMSEARCH="$MYPATH/ps.$SMREPO.$$.json"
-    if [ "$(psrelpreq "$(echo "$SMVER" | cut -f2 -d=)" \
-	    "$(echo "$SMVERNEW" | cut -f2 -d=)")" -eq 0 ] ; then
-        ADMINENABLED="$(yq -r '.admin.enabled' "$PSVALUES")"
-        ADMINS="$(yq -r '.admin.replicas' "$PSVALUES")"
-	ADMINR="$(bash "$MYPATH/../op/admin/pods.sh" | jq 'length')"
-	if [[ "$ADMINENABLED" = "true" ]] ; then
-	    if (( $ADMINR < 2 )) ; then
-	        bash "$MYPATH/../op/admin/scale.sh" 1
-	    fi
-	    if (( $ADMINS < 2 )) ; then
-	        ADMINS=2
-	    fi
-	fi
-	echo $ADMINENABLED $ADMINS $ADMINR
-	envbase ADMINS "$ADMINS"
-	cat "$PSVALUES" | bash "$MYPATH/../envs/common/ps/upgradeadmin1.sh" > "$PSVALUES".0
-	bash "$MYPATH/../envs/common/ps/newvalues.sh" > "$PSVALUES".1
-	if [[ "$ADMINENABLED" = "true" ]] ; then
-	    helm upgrade "$PSREL" "$SMREPO/server-components" -n "$PSNS" \
-	        -f "$PSVALUES".1 -f "$PSVALUES".0 $SMVERNEW \
-	        --description="upgrade administrative server pod to $SMVERNEW" \
-	        --timeout=30m --wait --debug > "$PSVALUES".debug
-        else
-           >&2 echo "administrative server is not enabled"
-        fi
-    else
-	>&2 echo pre-req check failed
-    fi
+    >&2 echo pre-requisites are not met, quit upgrading
 fi
