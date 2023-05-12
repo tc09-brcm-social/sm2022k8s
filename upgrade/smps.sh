e5d87e63b767158aff2b72ac619af628ea6f5b8b
Tommy Cheng <yihhwa.cheng@broadcom.com>
2023-05-12 01:27:00 +0000

    upgrade: smps env.shlib

--- upgrade/smps.sh
+++ upgrade/smps.sh
@@ -24,16 +24,20 @@ psrelpreq() {
 	>&2 echo upgrade to version not specified
 	echo 1
     else
-        bash "${MYPATH}/ps/values.sh"  > "$PSVALUES"
+        bash "${MYPATH}/../envs/common/sc/rtvalues.sh"  > "$PSVALUES"
 	helm search repo "$SMREPO" --versions -o json \
 	    | jq '[.[] | select(.name == "'$SMREPO/server-components'")]' \
 	    > "$HELMSEARCH"
 	tag="$(jq -r '.[] | select(.version == "'$_ver'") | .app_version' "$HELMSEARCH")"
 	newtag="$(jq -r '.[] | select(.version == "'$_newver'") | .app_version' "$HELMSEARCH")"
-	envbase SMTAG "$tag"
-	envbase SMNEWTAG "$newtag"
 	tag0="$(yq -r '.policyServer.tag' "$PSVALUES")"
-	if [[ "$tag" = "$tag0" ]] ; then
+	if [[ "$(yq -r '.policyServer.replicas' "$PSVALUES")" < "$PSMIN" ]] ; then
+	    >&2 echo running policy server pod replica is insufficient to continue, expect $PSMIN minimally
+	    echo 1
+	elif [[ "$tag" = "$tag0" ]] ; then
+	    envbase SMTAG "$tag"
+	    envbase SMNEWTAG "$newtag"
+	    >&2 yq '.policyServer.rollingUpdate' "$PSVALUES"
 	    echo 0
 	else
 	    >&2 echo runtime version $tag0 does not match with environment version $tag
@@ -41,6 +45,19 @@ psrelpreq() {
 	fi
     fi
     }
+
+#
+## uppspod
+#
+uppspod() {
+    bash ../envs/common/sc/newvalues.sh > "$PSVALUES".1
+    cat "$PSVALUES" | \
+	bash ../envs/common/sc/upgradeps1.sh  > "$PSVALUES".0
+    helm upgrade "$PSREL" "$SMREPO/server-components" -n "$PSNS" \
+        -f "$PSVALUES".1 -f "$PSVALUES".0 $SMVERNEW \
+        --description="upgrade policy server pod to $SMVERNEW" \
+        --timeout=30m --wait --debug > "$PSVALUES".debug
+}
 
 #
 ## Start Here
@@ -53,38 +70,19 @@ createns "$PSNS"
 #
 if [[ -z "$(relexist "$PSNS" "$PSREL")" ]] ; then
     >&2 echo SiteMinder Server Component release $PSREL is not installed in namespace $PSNS
-#    helm install "$PSREL" -n ${PSNS} \
-#        $SMREPO/server-components $SMVER -f "$PSVALUES" \
-#	--debug > "$PSREL.$PSNS.$$.debug"
-else
-    PSVALUES="$MYPATH/ps.$$.values.yaml"
-    HELMSEARCH="$MYPATH/ps.$SMREPO.$$.json"
-    if [ "$(psrelpreq "$(echo "$SMVER" | cut -f2 -d=)" \
-	    "$(echo "$SMVERNEW" | cut -f2 -d=)")" -eq 0 ] ; then
-        ADMINENABLED="$(yq -r '.admin.enabled' "$PSVALUES")"
-        ADMINS="$(yq -r '.admin.replicas' "$PSVALUES")"
-	ADMINR="$(bash "$MYPATH/../op/admin/pods.sh" | jq 'length')"
-	if [[ "$ADMINENABLED" = "true" ]] ; then
-	    if (( $ADMINR < 2 )) ; then
-	        bash "$MYPATH/../op/admin/scale.sh" 1
-	    fi
-	    if (( $ADMINS < 2 )) ; then
-	        ADMINS=2
-	    fi
-	fi
-	echo $ADMINENABLED $ADMINS $ADMINR
-	envbase ADMINS "$ADMINS"
-	cat "$PSVALUES" | bash "$MYPATH/../envs/common/ps/upgradeps1.sh" > "$PSVALUES".0
-	bash "$MYPATH/../envs/common/ps/newvalues.sh" > "$PSVALUES".1
-	helm upgrade "$PSREL" "$SMREPO/server-components" -n "$PSNS" \
-	    -f "$PSVALUES".1 -f "$PSVALUES".0 $SMVERNEW \
-	    --description="upgrade policy server pod to $SMVERNEW" \
-	    --timeout=30m --wait --debug > "$PSVALUES".debug
-    else
-	>&2 echo pre-req check failed
-    fi
-#    >&2 echo release $PSREL exists, attempt to upgrade
-#    helm upgrade --install "$PSREL" -n ${PSNS} \
-#        $SMREPO/server-components $SMVER -f "$PSVALUES" \
-#        --debug > "$PSREL.$PSNS.$$.debug"
+    exit 1
 fi
+if [[ "$(bash "$MYPATH/../envs/common/sc/rtvalues.sh" | yq -r '.policyServer.enabled')" != \
+    "true" ]] ; then
+    >&2 echo SiteMinder Policy Server Pod is not enabled, upgrade skipped.
+    exit
+fi
+PSVALUES="$MYPATH/ps.$$.values.yaml"
+HELMSEARCH="$MYPATH/ps.$SMREPO.$$.json"
+if [[ "$(psrelpreq "$(echo "$SMVER" | cut -d= -f2)" "$(echo "$SMVERNEW" | cut -d= -f2)")" == 0 ]] ; then
+  >&2 echo to upgrade
+  uppspod
+else
+  >&2 echo quite upgrade
+fi
+
