940f723378ec36dd1ee9034995765dd46be7fe8f
Tommy Cheng <yihhwa.cheng@broadcom.com>
2023-05-19 06:04:25 +0000

    envs/common/smmaint: rtvalues

---
+++ envs/common/smmaint/rtvalues.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+MYPATH=$(cd $(dirname "$0"); pwd)
+cd "${MYPATH}"
+. "${MYPATH}/../../../base/env.shlib"
+if [[ ! -z "$VERSHLIB" ]] ; then
+    . "${MYPATH}/../../../base/$VERSHLIB"
+fi
+helm get values "$SMMAINTREL" -n "$PSNS"
