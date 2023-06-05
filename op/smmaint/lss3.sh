e85e6d76b5d55bb7c3e2f9c35b330f1dc4668a5b
Tommy Cheng <yihhwa.cheng@broadcom.com>
2023-06-05 15:53:37 +0000

    op/smmaint: gets3, lss3, rms3

---
+++ op/smmaint/lss3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+MYPATH=$(cd $(dirname "$0"); pwd)
+cd "${MYPATH}/../.."
+. ./env.shlib
+cd "${MYPATH}"
+. "${MYPATH}/../../base/env.shlib"
+if [[ ! -z "$VERSHLIB" ]] ; then
+    . "${MYPATH}/../../base/$VERSHLIB"
+fi
+aws s3 ls "$S3URI$1"
