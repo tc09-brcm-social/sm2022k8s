ed2aeee7a331493782f6d2e0126d5dccc3a8030a
Tommy Cheng <yihhwa.cheng@broadcom.com>
2023-08-25 15:24:54 +0000

    envs/common/prometheus: pull versions

---
+++ envs/common/prometheus/pull.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+MYPATH=$(cd $(dirname "$0"); pwd)
+cd "${MYPATH}"
+. "${MYPATH}/../../../base/env.shlib"
+if [[ ! -z "$VERSHLIB" ]] ; then
+    . "${MYPATH}/../../../base/$VERSHLIB"
+fi
+if [[ ! -z "$1" ]] ; then
+    INGRESSVER="$1"
+fi
+helm pull "$PROMETHEUSREPO"/"$PROMETHEUSCHART" $PROMETHEUSVER
