c9282f3f6802148c009f000bf2aea80e85ab8412
Tommy Cheng <yihhwa.cheng@broadcom.com>
2023-09-08 11:15:36 +0000

    envs/ahdemo: prometheusop-values updated

--- envs/ahdemo/prometheusop-values.sh
+++ envs/ahdemo/prometheusop-values.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MYPATH=$(cd $(dirname "$0"); pwd)
 cd "${MYPATH}"
-bash ../common/prometheus/kube-prometheus-value.sh \
-    | bash ../common/prometheus/alertmanager1.sh \
+echo '{}' \
+    | bash ../common/prometheus/kube-prometheus-default.sh \
+    | bash ../common/prometheus/alertmanager2.sh \
     | bash ../common/prometheus/storageclass.sh
