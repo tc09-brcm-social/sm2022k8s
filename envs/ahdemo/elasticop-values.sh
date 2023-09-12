95417b543026e4e7ce2d994cebec81bcdf3fbd6a
Tommy Cheng <yihhwa.cheng@broadcom.com>
2023-09-12 12:55:46 +0000

    envs/ahdemo: env.shlib adding missing grafanaoprt

--- envs/ahdemo/elasticop-values.sh
+++ envs/ahdemo/elasticop-values.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MYPATH=$(cd $(dirname "$0"); pwd)
 cd "${MYPATH}"
-bash ../common/elastic/elasticop-values.sh
+echo -n ''
+#bash ../common/elastic/elasticop-values.sh
 #bash ../common/elastic-registry.sh \
 #    | bash ../common/elastic-min.sh
--- envs/ahdemo/grafanaop-values.sh
+++ envs/ahdemo/grafanaop-values.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MYPATH=$(cd $(dirname "$0"); pwd)
 cd "${MYPATH}"
-bash ../common/prometheus/grafana-operator-value.sh \
+#bash ../common/prometheus/grafana-operator-value.sh \
+echo '{}' \
     | bash ../common/prometheus/grafana-default.sh \
     | bash ../common/prometheus/grafana-ui.sh
